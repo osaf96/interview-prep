fc746478350186e8a870f55e3c507bb4d897ce39
shyamzzp <shyamsundersuthar009@gmail.com>
2022-06-09 16:40:29 +0530

    Add the changes for the gh-pages branch and deployment.

--- _website/docusaurus.config.js
+++ _website/docusaurus.config.js
@@ -1,17 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  title: 'interview-prep',
-  tagline: 'Notes regarding things I have learned',
-  url: 'https://interview-prep.shyamzzp.com',
-  baseUrl: '/',
-  favicon: 'favicon.ico',
-  organizationName: 'shyamzzp',
-  projectName: 'interview-prep',
+  title: "interview-prep",
+  tagline: "Notes regarding things I have learned",
+  url: "https://shyamzzp.github.io",
+  baseUrl: "/interview-prep/",
+  favicon: "favicon.ico",
+  organizationName: "shyamzzp",
+  projectName: "interview-prep",
   themeConfig: {
     navbar: {
-      title: 'Interview Preparation Notes',
-      
+      title: "Interview Preparation Notes",
+
       links: [
         // {
         //   href: 'https://github.com/shyamzzp/interview-prep',
@@ -24,41 +24,41 @@ module.exports = {
         //   position: 'left',
         // },
         {
-          href: 'https://github.com/shyamzzp/interview-prep',
-          position: 'right',
-          className: 'header-github-link navbar__item navbar__link',
-          'aria-label': 'GitHub repository',
+          href: "https://github.com/shyamzzp/interview-prep",
+          position: "right",
+          className: "header-github-link navbar__item navbar__link",
+          "aria-label": "GitHub repository",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       copyright: `copyright © ${new Date().getFullYear()} shyamzzp.`,
     },
     prism: {
-      theme: require('prism-react-renderer/themes/github'),
-      darkTheme: require('prism-react-renderer/themes/oceanicNext'),
+      theme: require("prism-react-renderer/themes/github"),
+      darkTheme: require("prism-react-renderer/themes/oceanicNext"),
     },
     disableDarkMode: false,
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '',
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
   ],
   plugins: [
-    path.resolve(__dirname, './extended-favicon-plugin'),
-    path.resolve(__dirname, './embedded-font-awesome-icons-plugin'),
-    path.resolve(__dirname, './collapse-sidebar-again-if-js-enabled-plugin'),
-    path.resolve(__dirname, './goatcounter-plugin'),
+    path.resolve(__dirname, "./extended-favicon-plugin"),
+    path.resolve(__dirname, "./embedded-font-awesome-icons-plugin"),
+    path.resolve(__dirname, "./collapse-sidebar-again-if-js-enabled-plugin"),
+    path.resolve(__dirname, "./goatcounter-plugin"),
   ],
 };
